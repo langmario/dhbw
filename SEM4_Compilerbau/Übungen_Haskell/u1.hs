71c46b05990fc5e029322a0d5f2933183959290e
Mario Lang <mariolang1@gmx.de>
2020-03-16 12:05:25 +0100

    restructured exercises

--- SEM4_Compilerbau/Übungen_Haskell/u1.hs
+++ SEM4_Compilerbau/Übungen_Haskell/u1.hs
@@ -1,22 +1,38 @@
 
--- Übung 1a
+-- Übung 1
+-- a
 lastElement (l : []) = l
 lastElement l = lastElement (tail l)
 
 
--- Übung 1b
+-- b
 stripList (_ : list) = reverse $ tail $ reverse list
 
 
--- Übung 1c
+-- c
 isPalindrom :: (Eq a) => [a] -> Bool
 isPalindrom [] = True
 isPalindrom (_ : []) = True
 isPalindrom list = if (head list) == (lastElement list) then isPalindrom $ stripList list else False
 
 
--- Übung 6a
+-- Übung 3
 curry3 :: ((a, b, c) -> d) -> (a -> b -> c -> d)
---uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
+-- uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
 
 curry3 f = \a -> \b -> \c -> f (a, b, c)
+
+
+
+-- Übung 6
+-- a
+data BTree a = emptyTree
+            | Node a (BTree a) (BTree a)
+            deriving Show;
+
+-- b
+mapTree :: BTree a -> (a -> b) -> BTree b
+mapTree (BTree a) f = 42
+
+
+-- c
